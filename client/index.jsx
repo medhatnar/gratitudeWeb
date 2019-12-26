426bc2a0f42dc831f8cc32db341ab950e1f03c21
narmander <narminshahin@gmail.com>
2019-12-26 18:48:41 -0500

    fixup! webpack set. react ready to go!

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-console.log(document.getElementById('main'))
+
 ReactDOM.render(
     <div>hi</div>,
 	document.getElementById('main')
