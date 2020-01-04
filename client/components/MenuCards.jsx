93395019a9203a6c136d3d82dab1e2fc45cc73f3
narmander <narminshahin@gmail.com>
2020-01-04 13:20:19 -0500

    html for menu page first go

--- client/components/MenuCards.jsx
+++ client/components/MenuCards.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export const MenuCards = ({ children, ...props }) => {
-	return (
-		<div class='container menu'>
-			
-		</div>
-	);
+export const MenuCards = ({
+	cardInfo = ['card', 'card2', 'card3'],
+	...props
+}) => {
+	return <>{cardInfo.map(info => {
+        return (
+            <div class="card">
+            <h3 class="card-number">
+            {info}
+            </h3>
+            <h2 class="card-title">{info}</h2>
+            </div>
+        )
+    })}</>;
 };
--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -3,11 +3,11 @@ import { MenuCards } from 'Components/MenuCards';
 
 const WelcomeText = props => {
 	return (
-		<div class='welcome-text'>
-			<h2>Daily Gratitude</h2>
-			<h1>{props.greeting}</h1>
-			<p>{props.infoText}</p>
-		</div>
+		<>
+			<h3 class="app-title">Daily Gratitude</h3>
+			<h1 class="greeting">{props.greeting}</h1>
+			<p class="blurb">{props.infoText}</p>
+		</>
 	);
 };
 
@@ -24,20 +24,26 @@ export const Welcome = props => {
 			</header>
 			<main role='main'>
 				<div class='container main-content'>
-					<WelcomeText
-						greeting='Take a minute to meditate and a moment to reflect'
-						infoText='An application with various calming sounds to meditate to and a personal
+					<div class='welcome-text'>
+						<WelcomeText
+							greeting='Take a minute to meditate and a moment to reflect'
+							infoText='An application with various calming sounds to meditate to and a personal
 				diary of all things in your life you are grateful for. Start your day
-				peacefully and thankfully.'
-					/>
-                <MenuCards />
+				positively.'
+						/>
+					</div>
+					<div class='container menu'>
+						<MenuCards {...props} />
+					</div>
 				</div>
 			</main>
 			{/* splash image */}
 			<div class='container splash-image'>
 				<figure>
 					<img src='some_calming_image.jpg' alt='some calming image' />
-					<figcaption>Visual explanation of an image for accessibility</figcaption>
+					<figcaption>
+						Visual explanation of an image for accessibility
+					</figcaption>
 				</figure>
 			</div>
 		</div>
