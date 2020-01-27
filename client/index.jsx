86dd08ddd490573e04634654fb75490341e179eb
Blessing Ajayi-Martins <blessmartins@gmail.com>
2020-01-27 12:40:33 -0500

    wrapped welcome component in browser-router

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import { Welcome } from 'Components/Welcome';
 
 ReactDOM.render(
-    <Welcome />,
+    <BrowserRouter>
+        <Welcome />
+    </BrowserRouter>
+    ,
 	document.getElementById('main')
 );
