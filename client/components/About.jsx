a395225c7abc23a7be47c7d97f9a837d0199f129
Blessing Ajayi-Martins <blessmartins@gmail.com>
2020-01-27 13:47:40 -0500

    plagiarized paragraph from headspace about page lol

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export const About = () => {
   return (
     <div>
-      <p>coming soon</p>
+      <div>
+        <h4>(I 100% took this from headspace... just wanted text to work with)It’s hard to talk about Headspace without talking about Andy Puddicombe. So let’s start there. In his early twenties, Andy cut his Sports Science degree short to become a Buddhist monk. For over 10 years, his meditation training took him across the world to Nepal, India, Burma, Thailand, Australia and Russia. Eventually, he was ordained at a Tibetan monastery in the Indian Himalayas.</h4>
+      </div>
+      
+      <div>
+        <h4>After completing his monastic commitment, Andy returned to the UK with the huge-yet-simple goal of teaching meditation and mindfulness to as many people as possible. To demystify the mystical, Andy set up a meditation consultancy and began working with politicians, athletes (that sports science background finally came in handy), and business leaders.</h4>
+      </div>
+      
     </div>
   )
   
