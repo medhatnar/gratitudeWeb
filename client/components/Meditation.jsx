fbded62c7d5d71c1dc4d623db29a02ffa3a0995a
narmander <narminshahin@gmail.com>
2020-03-06 13:05:16 -0500

    fixup^

--- client/components/Meditation.jsx
+++ client/components/Meditation.jsx
@@ -4,7 +4,21 @@ import styled from 'styled-components';
 let mp3 = 'https://dailygratitudemeditation.s3.amazonaws.com/nature.mp3';
 
 export const Meditation = () => {
-	const [duration, setDuration] = useState(10);
+    const [duration, setDuration] = useState(10);
+    
+    function fadeInOut(interval) {
+        let newVolume = myAudio.volume;
+        const changeVol = setInterval(() => {
+            var my_math = (Math.exp(newVolume) - 1) / (Math.E - 1);
+            if (my_math < 0 || my_math > 1) {
+                clearInterval(changeVol);
+                if (my_math < 0) myAudio.pause();
+            } else {
+                myAudio.volume = my_math;
+                newVolume += interval;
+            };
+        }, 50);
+    }
 
 	var myAudio = new Audio(mp3);
 	myAudio.volume = 0;
