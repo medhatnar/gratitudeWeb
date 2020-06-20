a6cb9ccf97420be3babc90992926d51e5ae60943
narmander <narminshahin@gmail.com>
2020-06-19 21:46:45 -0400

    new templates who dis

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -1,8 +1,7 @@
 """Routes for logged-in account pages."""
 from flask import Blueprint, render_template
 
-app_bp = Blueprint('app_bp', __name__,
-                   template_folder='templates', static_folder='../../dist')
+app_bp = Blueprint('app_bp', __name__, template_folder='../../dist')
 
 
 @app_bp.route('/')
