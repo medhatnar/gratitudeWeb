59ce9fac2c06cf851b7e05ca6cc66fe211334128
narmander <narminshahin@gmail.com>
2020-06-22 11:51:56 -0400

    fixup^

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
-		publicPath: '/dist/',
+		publicPath: '/',
 	},
 	devServer: {
 		contentBase: path.resolve(__dirname, 'dist'),
