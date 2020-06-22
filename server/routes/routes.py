b954337a1e54108792c70f22804aa6cb171e5e0e
narmander <narminshahin@gmail.com>
2020-06-22 12:03:56 -0400

    fixup^

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template
 
 app_bp = Blueprint('app_bp', __name__,
-                   template_folder='templates', static_folder='dist')
+                   template_folder='../templates', static_folder='dist', static_url_path="")
 
 
 @app_bp.route('/')
