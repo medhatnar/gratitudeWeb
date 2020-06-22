bc17eb2fe0dc2fd3745d8f94fbd58eca278e1105
narmander <narminshahin@gmail.com>
2020-06-22 15:52:58 -0400

    idk

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -1,7 +1,7 @@
 """Routes for logged-in account pages."""
 from flask import Blueprint
 
-app_bp = Blueprint('app_bp', __name__, static_folder='../../dist', static_url_path='')
+app_bp = Blueprint('app_bp', __name__, static_folder='../../dist', static_url_path='/')
 
 
 @app_bp.route('/')
